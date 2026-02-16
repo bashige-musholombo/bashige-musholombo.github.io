f5bb00a8f7162c026ec8243bffe0bdd1396309f0
bashige-musholombo <bashigesalim@gmail.com>
2026-02-16 16:10:14 +0000

    Deploying to gh-pages from @ bashige-musholombo/bashige-musholombo.github.io@efa7211b05c300edf0702f03a405818896cfc6d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-links",
+          title: "Links",
+          description: "Below, I&#39;ll be sharing links to resources that I find particularly useful.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
