485b06e9172396c9cd1265333bd38f1966a960b0
bashige-musholombo <bashigesalim@gmail.com>
2026-02-17 09:54:03 +0000

    Deploying to gh-pages from @ bashige-musholombo/bashige-musholombo.github.io@bc7c13b38ef9e85d619132acfdf94f07e47f25ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,19 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/Curriculum/";
-          },
         },{id: "nav-links",
           title: "Links",
           description: "Below, I&#39;ll be sharing links to resources that I find particularly useful. If you&#39;re a student, you might find them useful (or not).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/links/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Curriculum/";
           },
         },{id: "nav-teaching",
           title: "Teaching",
