1fbe23e556dd8aeef303ddcb3f7c53810a3efac5
bashige-musholombo <bashigesalim@gmail.com>
2026-02-19 20:36:39 +0000

    Deploying to gh-pages from @ bashige-musholombo/bashige-musholombo.github.io@1b7eb3c309579e3cc6c10401e15eedaac79f3a58 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "Research",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/research/";
-          },
-        },{id: "nav-teaching",
+  },{id: "nav-teaching",
           title: "Teaching",
           description: "",
           section: "Navigation",
